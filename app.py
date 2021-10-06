a61136b2ca327750822225e193c6a34cb5a124c6
Athus Vieira <athus.vieira@wexinc.com>
2021-10-06 15:10:19 -0300

    public host

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
 
     # start the application
     app.run(
+        host='0.0.0.0',
         debug=os.environ.get('FLASK_ENV', default='development') == 'development',
         port=int(os.environ.get("PORT", 5000))
     )
