ad7aca01fd70ed18f477b9765896b35f27ab451f
Athus Vieira <athus.vieira@wexinc.com>
2021-10-06 14:40:54 -0300

    run app in dev mode if FLASK_ENV is set to development

--- app.py
+++ app.py
@@ -25,4 +25,7 @@ if __name__ == '__main__':
     register_blueprints()
 
     # start the application
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        debug=os.environ.get('FLASK_ENV', default='development') == 'development',
+        port=int(os.environ.get("PORT", 5000))
+    )
